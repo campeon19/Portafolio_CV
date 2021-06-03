880cfebc2d4681cb19c95eb090f471d0ecbff858
campeon19 <53914171+campeon19@users.noreply.github.com>
2021-06-02 19:14:28 -0600

    Creacion de una aplicacion from scratch e implementacion de navbar

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+import Navbar from './Navbar/Navbar';
+import Home from './Home/Home';
+import Works from './Works/Works';
+import Contact from './Contact/Contact';
+
+function App(){
+    return(
+        <div>
+            <Navbar></Navbar>
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    publicPath: '/',
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          MiniCssExtractPlugin.loader,
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets',
+          },
+        },
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /(node_modules)/,
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+};
